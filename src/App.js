c38049966a533acfc159eb7aa7b6ea0019cbc475
dynatyper <fushichou8@gmail.com>
2021-05-24 16:16:48 -0400

    Initial project creation. Add Die component and FA icons.

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Die from './Die';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Die face='five' />
+      <Die face='four' />
+      <Die face='three' />
+    </div>
+  );
+}
+
+export default App;
